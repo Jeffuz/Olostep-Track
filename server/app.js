40b2d3ba1d32f804fd7a4047f784d2d930482824
mr-loop-1 <loopolecreator1@gmail.com>
2024-08-18 12:13:22 +0530

    add route and package

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const router = require("./routes/index");
+
 const bodyParser = require("body-parser");
 
 require("./database");
