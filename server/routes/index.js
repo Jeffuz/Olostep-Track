02a5db12ab58c9f9e243a4f3d8e1c2a173071315
mr-loop-1 <loopolecreator1@gmail.com>
2024-08-18 12:44:27 +0530

    organise the files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const scrapeModel = require("../model/scrape_result");
 
 const router = express.Router();
+
+router.post(async (req, res) => {
+    try {
+        const scrapedData = scrapeWebpage(req.body.url);
+
+        const createWebpage = new scrapeModel({
+            title: scrapedData.title,
+        ///
+        });
+
+        await createWebpage.save();
+
+        // after saving we will return the result
+
+        // to be transformed if not yet
+        return res.json(scrapedData);
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(500).json({ error: "Server Error" });
+    }
+    
+});
+// router.post("process", processesDataScrapingService)
 
 module.exports = router;
