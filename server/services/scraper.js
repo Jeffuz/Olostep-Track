ee90b283de0ccc773437f324a761b2ff745f0226
mr-loop-1 <loopolecreator1@gmail.com>
2024-08-18 19:29:20 +0530

    puppet fix

--- server/services/scraper.js
+++ server/services/scraper.js
@@ -20,7 +20,9 @@ async function scrapeWebpage(url, options = {}) {
 let browser;
   try {
     // Launch the browser
-     browser = await puppeteer.launch(); 
+     browser = await puppeteer.launch({
+        headless: true
+     }); 
     const page = await browser.newPage();
 
 
